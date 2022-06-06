8bb676430874030476d991207d2794c4f6837ad6
Emir-1800 <m.emir1800@gmail.com>
2022-06-06 17:14:53 +0600

    Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import PrimarySearchAppBar from "./Components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <PrimarySearchAppBar />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
