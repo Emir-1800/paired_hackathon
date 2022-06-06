3ed80687f91bba33cc787e59fb153b0912df39b9
Emir-1800 <m.emir1800@gmail.com>
2022-06-06 14:03:58 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import Navcom from "./Components/Navbar/Navbar";
+
 const App = () => {
-  return <div>Salam</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navcom />
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
