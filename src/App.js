d42baea0bc17b37de89fd1b2bdedd6e6d430caaf
Nuris2 <nurislam.murzaliev01@gmail.com>
2022-06-06 15:20:39 +0600

    Home

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import Navcom from "./Components/Navbar/Navbar";
+import Home from "./Components/Navbar/Home/Home";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Navcom />
+        <Home />
       </BrowserRouter>
     </>
   );
