94542dd50d4052b382976d7e75b00a6850cb7321
Nuris2 <nurislam.murzaliev01@gmail.com>
2022-06-08 16:53:43 +0600

    before pull

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,11 +1,15 @@
+// import { Route } from "@mui/icons-material";
+import { Home } from "@mui/icons-material";
 import React from "react";
-import { Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import AddProduct from "./Components/Admin/AddProduct/AddProduct";
 
 const MainRoutes = () => {
   return (
-    <div>
-      <Routes></Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/add" element={<AddProduct />} />
+    </Routes>
   );
 };
 
